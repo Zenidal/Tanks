be6f5ae29119d64faee4b288035981fe279f99fb
Zenidal <1ochka1994@mail.ru>
2022-02-23 23:27:36 +0300

    init commit

---
+++ Source/Homework2/TanksController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TanksController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOMEWORK2_API ATanksController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
